aeddae376813baba047d631fb560020fb59b6183
dragon448795 <dragon448795@gmail.com>
2026-02-12 15:25:26 +0800

    Create __init__.py

---
+++ rpg_system/commands/__init__.py
@@ -0,0 +1,30 @@
+"""
+RPG 指令註冊中心
+"""
+from .system import setup_system_commands
+from .character import setup_character_commands
+from .inventory import setup_inventory_commands
+from .house import setup_house_commands
+from .party import setup_party_commands
+
+async def register_all_commands(tree, rpg):
+    """
+    註冊所有 RPG 指令
+    """
+    # 建立主群組
+    rpg_group = app_commands.Group(
+        name="rpg",
+        description="🎮 RPG 冒險系統"
+    )
+    
+    # 註冊各個子系統的指令
+    setup_system_commands(rpg_group, rpg)
+    setup_character_commands(rpg_group, rpg)
+    setup_inventory_commands(rpg_group, rpg)
+    setup_house_commands(rpg_group, rpg)
+    setup_party_commands(rpg_group, rpg)
+    
+    # 加到指令樹
+    tree.add_command(rpg_group)
+    print("✅ RPG 系統：所有指令註冊完成")
+    return 1
